1a44ab9218989fb853b59a9dd9d0ae05d40cb781
Luca Armaroli <lukaarma@gmail.com>
2020-10-14 01:40:12 +0200

    modified the fake test app
    Co-authored-by: Lorenzo Romio <lorenzo.romio@live.it>

--- src/app/script/index.js
+++ src/app/script/index.js
@@ -1,13 +1,55 @@
-window.onload(makeJson());
-
-function makeJson() {
+document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementsByClassName('testForm')[0];
     form.addEventListener('submit', handleFormSubmit);
-}
+});
 
-const handleFormSubmit = (event) => {
-    // Stop the form from submitting since we’re handling that with AJAX.
+const handleFormSubmit = async (event) => {
+    // Stop the form from submitting
     event.preventDefault();
 
-    console.log(JSON.stringify(form, null, 4));
+    const form = document.getElementsByClassName('testForm')[0];
+    const body = JSON.stringify(toObject(form));
+    console.log(body);
+
+    const response = await fetch('http://localhost:6789/users/login', {
+        method: 'POST',
+        body: body
+      });
+      console.log(await response.json());
+};
+
+
+
+function toObject(form) {
+    var obj = Object.create(null);
+
+    function assign(name, value) {
+        if (obj[name] === undefined) {
+            obj[name] = value;
+        } else if (Array.isArray(obj[name])) {
+            obj[name].push(value);
+        } else {
+            obj[name] = [obj[name], value];
+        }
+    }
+
+    Array.prototype.slice.call(form.elements).forEach(function (field) {
+        if (field.name && !field.disabled && (['file', 'reset', 'button'].indexOf(field.type) === -1)) {
+            if (field.type === 'select-multiple') {
+                Array.prototype.slice.call(field.options).forEach(function (option) {
+                    if (option.selected) {
+                        assign(field.name, option.value);
+                    }
+                })
+            } else if (field.type === 'checkbox' || field.type === 'radio') {
+                if (field.checked) {
+                    assign(field.name, field.value);
+                }
+            } else {
+                assign(field.name, field.value);
+            }
+        }
+    })
+
+    return obj;
 };
