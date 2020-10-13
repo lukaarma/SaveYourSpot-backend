1ef0b49b93627b7c96a488407710ba17d3d8fa06
Luca Armaroli <lukaarma@gmail.com>
2020-10-13 21:14:58 +0200

    moved databse to Atlas

--- src/utils/Databse.ts
+++ src/utils/Databse.ts
@@ -11,16 +11,15 @@ export async function initDb(): Promise<void> {
             user: process.env.DB_USER ?? '',
             password: process.env.DB_PASSWORD ?? ''
         },
-        authSource: process.env.DB_ROLE,
         useCreateIndex: true,
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: false
     };
 
-    const dbConnection = `mongodb://${process.env.DB_SERVER}`;
+    const dbConnection = process.env.DB_SERVER || 'mongodb://localhost/SaveYourSpot';
 
-    logger.verbose(`[DATABSE] connection string '${dbConnection}'`);
+    logger.debug(`[DATABSE] connection string '${dbConnection}'`);
 
     logger.info('Connecting to database...');
     await mongoose.connect(dbConnection, mongooseOptions);
