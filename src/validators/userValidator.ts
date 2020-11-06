4739d5b8f2f09832bc9f1060cb29d510e25d9dca
Luca Armaroli <lukaarma@gmail.com>
2020-11-06 15:16:00 +0100

    moved Joi validator fixed typos and regexes
    Co-authored-by: Lorenzo Romio <lorenzo.romio@live.it>

---
+++ src/validators/userValidator.ts
@@ -0,0 +1,33 @@
+import customJoi from './extendedJoi';
+
+const validateUser = customJoi.object({
+    email: customJoi.string()
+        .trim()
+        .pattern(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9][a-zA-Z0-9-]{1,252}(?:\.[a-zA-Z0-9]{2,})+$/),
+
+    password: customJoi.string()
+        .trim()
+        .min(8)
+        .pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])([a-zA-Z\d$@!%*?&]{8,}$)$/),
+
+    repeatPassword: customJoi.ref('password'),
+
+    firstName: customJoi.string()
+        .trim()
+        .pattern(/^[\p{L}][ \p{L}'-]*[\p{L}]$/u),
+
+    lastName: customJoi.string()
+        .trim()
+        .pattern(/^[\p{L}][ \p{L}'-]*[\p{L}]$/u),
+
+    phoneNumber: customJoi.string()
+        .trim()
+        .phoneNumber(),
+
+    birthdate: customJoi.date()
+        // min 14 years old
+        .max(new Date(Date.now() - 14 * 365 * 24 * 60 * 60 * 1000)),
+});
+
+
+export default { validateUser };
